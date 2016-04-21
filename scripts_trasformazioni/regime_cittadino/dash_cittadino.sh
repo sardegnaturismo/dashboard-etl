97f852ed75d85e5eaeb725a1caa8acb95e9dc84d
gianlucaparlangeli <gianluca.parlangeli@extrasys.it>
2016-04-21 12:49:20 +0200

    Log persistenti su disco
    Gestione archiviazione log

--- scripts_trasformazioni/regime_cittadino/dash_cittadino.sh
+++ scripts_trasformazioni/regime_cittadino/dash_cittadino.sh
@@ -1,1 +1,10 @@
-nohup /opt/pentaho/data-integration/kitchen.sh /rep:"sired_pdi_repo" /job:"J_DASH_CITTADINO" /dir:/SIRED/DASHBOARD/CITTADINO /user:admin /pass:admin /level:Basic > /opt/pentaho/log_caricamenti/dash_cittadino.txt
+#!/bin/bash
+DIM=$( ls -l /opt/pentaho/log_caricamenti/dash_cittadino.log | awk -F' ' '{print $5}' )
+DATA=$(date +%Y%m%d)
+#Se il file supera i 10M crea un archivio
+if [ $DIM -gt 10485760 ]
+        then
+                tar -czvf /opt/pentaho/log_caricamenti/dash_cittadino.log.${DATA}.tgz /opt/pentaho/log_caricamenti/dash_cittadino.log
+                rm -f /opt/pentaho/log_caricamenti/dash_cittadino.log
+fi
+nohup /opt/pentaho/data-integration/kitchen.sh /rep:"sired_pdi_repo" /job:"J_DASH_CITTADINO" /dir:/SIRED/DASHBOARD/CITTADINO /user:admin /pass:admin /level:Basic >> /opt/pentaho/log_caricamenti/dash_cittadino.log
