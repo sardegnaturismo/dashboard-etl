72079a876a9af6a81f0d5a7dac60cb3841114641
gianlucaparlangeli <gianluca.parlangeli@extrasys.it>
2016-04-21 16:06:03 +0200

    Aggiunti parametri MongoDB

--- create_properties.sh
+++ create_properties.sh
@@ -1,1 +1,1 @@
-${PENTAHO_HOME}/data-integration/pan.sh -file="${PENTAHO_HOME}/setup/kettle_properties.ktr" -level=Minimal ${POSTGRES_IP} ${POSTGRES_PORT} ${POSTGRES_USR} ${POSTGRES_PSW} ${POSTGRES_IP} ${POSTGRES_PORT} ${POSTGRES_USR} ${POSTGRES_PSW} ${ELASTIC_IP}:${ELASTIC_PORT} ${PENTAHO_HOME}/.kettle/
+${PENTAHO_HOME}/data-integration/pan.sh -file="${PENTAHO_HOME}/setup/kettle_properties.ktr" -level=Minimal ${POSTGRES_IP} ${POSTGRES_PORT} ${POSTGRES_USR} ${POSTGRES_PSW} ${MONGO_IP} ${MONGO_PORT} ${MONGO_DB} ${ELASTIC_IP}:${ELASTIC_PORT} ${PENTAHO_HOME}/.kettle/
