a1dc280a712e306db97315387210b1ea8af5f664
Luca Lusso <llusso@wellnet.it>
2016-05-23 17:57:22 +0200

    Caricate variabili d'ambiente da file esterno

--- startup.sh
+++ startup.sh
@@ -5,5 +5,6 @@ PATH="/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/opt/aws/bin:
 
 export AWS_PATH
 export PATH
+cd /data && ./settings.sh
 
 cd /data/repo/kettle && docker-compose up -d
