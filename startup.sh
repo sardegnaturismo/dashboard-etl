ea37a9b52129f9da9dca591c977eceeda4f51da9
Luca Lusso <llusso@wellnet.it>
2016-05-23 18:58:55 +0200

    Separati docker-compose di kettle e mongo e aggiunto script di shutdown

--- startup.sh
+++ startup.sh
@@ -3,8 +3,14 @@
 AWS_PATH="/opt/aws"
 PATH="/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/opt/aws/bin:/home/ec2-user/.local/bin:/home/ec2-user/bin"
 
+# Settings varables.
 export AWS_PATH
 export PATH
 cd /data && ./settings.sh
 
-cd /data/repo/kettle && docker-compose up -d
+# Run containers
+cd /data/repo/kettle && docker-compose --file docker-compose-mongo.yml up -d
+cd /data/repo/kettle && docker-compose up
+
+# Shutdown the server.
+sudo shutdown +10 "ETL finished; halt in 10 minutes"
