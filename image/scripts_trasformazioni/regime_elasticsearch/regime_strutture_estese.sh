f6cf74ecdc7f74c8c4ae196a3fb58b9bb081a52c
gianlucaparlangeli <gianluca.parlangeli@extrasys.it>
2017-06-26 11:34:15 +0200

    Aggiunto job di quadratura per strutture estese

--- image/scripts_trasformazioni/regime_elasticsearch/regime_strutture_estese.sh
+++ image/scripts_trasformazioni/regime_elasticsearch/regime_strutture_estese.sh
@@ -5,6 +5,7 @@
 INDEX="strutture_estese"
 TYPE="ricettive"
 LOGFILE_JOB="/opt/pentaho/log_caricamenti/regime_strutt_estese"
+LOGFILE_QUAD="/opt/pentaho/log_caricamenti/quad_strutt_estese"
 DATA=$(date +%Y/%m/%d)
 
 echo "INIZIO STRUTTURE ESTESE $(date)"
@@ -31,6 +32,7 @@ fi
 echo "#JOB ETL1"
 RETRY=0
 CONNECTION=0
+OK=0
 
 while [ $CONNECTION -lt 4 ] && [ $RETRY -lt 2 ]; do
 
@@ -77,7 +79,37 @@ if [ $CONNECTION -eq 4 ]
         	deleteWork $INDEX
 		exit 1   
  else
-	changeAliasToLive $INDEX
+        echo "inizio JOB QUADRATURA1"
+	while [ $OK -lt 2 ]; do
+	        if [ -s ${LOGFILE_QUAD}.log ]
+	         then mv ${LOGFILE_QUAD}.log ${LOGFILE_QUAD}_$(date +%H%m).log
+        	fi
+
+		nohup /opt/pentaho/data-integration/kitchen.sh /rep:"${POSTGRES_REPO}" /job:"J_QUAD_MONGO_ELASTIC_STRUT" /dir:/SIRED/QUERY_CHECK/STRUT_ESTESE_MONGO_ELASTIC /user:admin /pass:admin /level:Basic &>> ${LOGFILE_QUAD}.log
+	
+		grep "An\ error\ occured\ loading\ the\ directory\ tree\ from\ the\ repository" ${LOGFILE_QUAD}.log
+		if [ $? -eq 0 ]
+		 then let "OK=$OK + 1"
+		 else break
+        	fi
+	done
+	if [ $OK -eq 2 ]
+         then echo -e "Indice : strutture_estese \n Job : J_QUAD_MONGO_ELASTIC_STRUT \n Tentativi retry job esauriti : repo non letto " | mail -s " DASHBOARD CITTADINO - Aggiornamento fallito " $DESTINATARI
+        	sleep 5
+        	exit 1
+	 else
+	        sleep 10
+        	changeAliasToLive $INDEX
+	fi
+fi
+
+#analizzo file di output di JOB QUADRATURA1, se fallito invio mail e stoppo il container
+ls /opt/pentaho/check_files/quadratura_ko
+if [ $? -eq 0 ]
+ then
+	echo -e "Indice : strutture_estese \n Job : J_QUAD_MONGO_ELASTIC_STRUT \n I dati non sono allineati " | mail -s " DASHBOARD CITTADINO - Aggiornamento fallito " $DESTINATARI
+	sleep 5
+	exit 1
 fi
 
 echo "STR EST FINITO $(date)"
